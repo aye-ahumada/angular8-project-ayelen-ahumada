c80df0c2254bdeade6cf51b21a2d17ddc12bf4b7
aye-ahumada <56736039+aye-ahumada@users.noreply.github.com>
2020-03-30 20:36:37 -0300

    Agregado canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 //COMPONENTES
 import { LoginComponent } from './login/login.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
-// import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'usuario/:index', component: UsuarioComponent  },
-  { path: 'usuarios', component: UsuariosComponent }
-  //{ path: '**', component:  }
+  { path: 'usuario/:index', component: UsuarioComponent, canActivate: [AuthGuard] },
+  { path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
